fefd92f1c07b17804fcf9198771e84317678d963
hari3303 <101964341+hari3303@users.noreply.github.com>
2023-12-30 15:57:37 +0530

    ew

--- 1_devops_infra/1_hub_and_spoke_basic_modules/1_network/9_backend_terraform/1main.tf
+++ 1_devops_infra/1_hub_and_spoke_basic_modules/1_network/9_backend_terraform/1main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "hub_terraform_state" {
-  bucket = "var.bucketname"  # Specify a globally unique bucket name
-  acl    = "var.acl"  # Set the ACL (Access Control List) for the bucket
+  bucket = var.bucketname  # Specify a globally unique bucket name
+  acl    = var.acl  # Set the ACL (Access Control List) for the bucket
 
   versioning {
     enabled = var.versioning  # Enable versioning for the bucket
